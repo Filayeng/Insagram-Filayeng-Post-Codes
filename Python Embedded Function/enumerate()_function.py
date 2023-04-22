b7bb43d5ecfa37f712ffc2faa02ec7051bb417fd
Filayeng <64534466+Filayeng@users.noreply.github.com>
2023-04-22 13:43:23 +0300

    Update enumerate()_function.py

--- Python Embedded Function/enumerate()_function.py
+++ Python Embedded Function/enumerate()_function.py
@@ -1,1 +1,12 @@
+#enumerate(iterable, start)
+
+lang = ["C++", "Python", "Java"]
+
+enu = enumerate(iterable = lang, start = 1)
+
+# print(list(enu))
+
+for count, itera in enumerate(enu):
+    print(itera)
+
 
